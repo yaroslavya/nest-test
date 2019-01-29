ff33f0e961ceebe477286f8d34a8c5cb1d00de09
Iaroslav Iakovlev <Iaroslav.Iakovlev@pbs.one>
2019-01-29 16:23:03 +0100

    mod changed readmemd to trigger the build

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("things are happening");
+  console.log("things are hopefully happening");
+  
 }
 bootstrap();
