56d42afc48ef9597491a9921cb3303b95c1b640b
Iaroslav Iakovlev <Iaroslav.Iakovlev@pbs.one>
2019-01-30 17:11:25 +0100

    mod: changed some stuff to trigger the build

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("things are hopefully happening");
+  console.log("code is fresh");
   
 }
 bootstrap();
